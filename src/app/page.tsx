abbe832f8f4cccaac43bf3e1c07ccca25998a4cf
ariuka-09 <ariuntuguldur3@gmail.com>
2026-05-15 23:19:55 +0800

    further changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" flex items-center justify-center h-screen bg-violet-400 ">
+    <div className=" flex items-center flex-col justify-center h-screen bg-violet-400 ">
       <p>Somethings cooking right here...</p>
-      <p>Bilguun pp so small</p>
+      <p>Bilguun pp not so small</p>
+      <p>Bendhoven Sodoo</p>
+      <p>Naba wasnt here</p>
     </div>
   );
 }
